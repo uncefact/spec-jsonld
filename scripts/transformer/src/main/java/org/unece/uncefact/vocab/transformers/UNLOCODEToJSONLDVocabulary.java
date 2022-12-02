d3856174c0ae787e8ee63eb42b291941d537b2b5
Kseniya Shychko <k.shychko@gmail.com>
2022-12-02 13:08:41 +0100

    fix: set @type for properties in unlocode vocab context

--- scripts/transformer/src/main/java/org/unece/uncefact/vocab/transformers/UNLOCODEToJSONLDVocabulary.java
+++ scripts/transformer/src/main/java/org/unece/uncefact/vocab/transformers/UNLOCODEToJSONLDVocabulary.java
@@ -21,19 +21,19 @@ public class UNLOCODEToJSONLDVocabulary extends Transformer {
 
     protected static String FUNCTION_CLASS_NAME = "Function";
     protected static String FUNCTION_CLASS = StringUtils.join(UNLOCODE_VOCAB_NS, ":", FUNCTION_CLASS_NAME);
-    public static String FUNCTIONS_PROPERTY_NAME = "functions";
+    public static final String FUNCTIONS_PROPERTY_NAME = "functions";
     public static String FUNCTIONS_PROPERTY = StringUtils.join(UNLOCODE_VOCAB_NS, ":", FUNCTIONS_PROPERTY_NAME);
     public static String SUBDIVISION_CLASS_NAME = "Subdivision";
     public static String SUBDIVISION_CLASS = StringUtils.join(UNLOCODE_VOCAB_NS, ":", SUBDIVISION_CLASS_NAME);
-    public static String COUNTRY_SUBDIVISION_PROPERTY_NAME = "countrySubdivision";
+    public static final String COUNTRY_SUBDIVISION_PROPERTY_NAME = "countrySubdivision";
     public static String COUNTRY_SUBDIVISION_PROPERTY = StringUtils.join(UNLOCODE_VOCAB_NS, ":", COUNTRY_SUBDIVISION_PROPERTY_NAME);
-    public static String SUBDIVISION_TYPE_PROPERTY_NAME = "subdivisionType";
+    public final static String SUBDIVISION_TYPE_PROPERTY_NAME = "subdivisionType";
     public static String SUBDIVISION_TYPE_PROPERTY = StringUtils.join(UNLOCODE_VOCAB_NS, ":", SUBDIVISION_TYPE_PROPERTY_NAME);
     public static String COUNTRY_CLASS_NAME = "Country";
     public static String COUNTRY_CLASS = StringUtils.join(UNLOCODE_VOCAB_NS, ":", COUNTRY_CLASS_NAME);
     public static String UNLOCODE_CLASS_NAME = "UNLOCODE";
     public static String UNLOCODE_CLASS = StringUtils.join(UNLOCODE_VOCAB_NS, ":", UNLOCODE_CLASS_NAME);
-    public static String PROPERTY_COUNTRY_CODE_NAME = "countryCode";
+    public final static String PROPERTY_COUNTRY_CODE_NAME = "countryCode";
     public static String PROPERTY_COUNTRY_CODE = StringUtils.join(UNLOCODE_VOCAB_NS, ":", PROPERTY_COUNTRY_CODE_NAME);
 
 
@@ -377,7 +377,19 @@ public class UNLOCODEToJSONLDVocabulary extends Transformer {
 
         for (String key : vocabGraph.keySet()) {
             jsonldVocabulary.getGraphJsonArrayBuilder().add(vocabGraph.get(key));
-            jsonldContext.getContextObjectBuilder().add(key, Json.createObjectBuilder(Map.of(ID, StringUtils.join(UNLOCODE_VOCAB_NS, ":", key))).build());
+            JsonObjectBuilder propertyObjectBuilder = Json.createObjectBuilder(Map.of(ID, StringUtils.join(UNLOCODE_VOCAB_NS,":", key)));
+            switch (key){
+                case PROPERTY_COUNTRY_CODE_NAME:
+                case COUNTRY_SUBDIVISION_PROPERTY_NAME:
+                case FUNCTIONS_PROPERTY_NAME:
+                    propertyObjectBuilder.add(TYPE, "@vocab");
+                    break;
+                case SUBDIVISION_TYPE_PROPERTY_NAME:
+                    propertyObjectBuilder.add(TYPE, "xsd:string");
+                    break;
+            }
+
+            jsonldContext.getContextObjectBuilder().add(key, propertyObjectBuilder.build());
         }
         vocabularies.add(jsonldVocabulary);
         contexts.add(jsonldContext);
